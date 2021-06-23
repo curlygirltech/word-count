96a8eb73bf8aa6d47600e8d7f2108e7a3064d21a
jcameron10 <jovonnec@gmail.com>
2021-06-22 22:19:21 -0400

    word counter logic

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-
+var count = document.getElementById('count');
+var input = document.getElementById('input');
 
 var input = document.querySelectorAll('textarea')[0];
+var words = input.value.match(/\b[-?(\w+)?]+\b/gi);
+var sentences = input.value.split(/[.|!|?]+/g);
+
+input.addEventListener('keyup', function(e) {
+
+  // word counter logic
+  wordCounter(e.target.value);
+  // sentence count logic
+
+  // reading time calculation
+
+  // keyword finding logic
+
+});
+function wordCounter(text) {
+  var text = input.value;
+  var wordCount = 0;
+  for (var i = 0; i <= text.length; i++) {
+    if (text.charAt(i) == ' ') {
+      wordCount++;
+    }
+  }
+  count.innerText = wordCount;
+}
